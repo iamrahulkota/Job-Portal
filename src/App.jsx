ae2a151eb27cb1af81003a8ced05fc104f029593
rahul-adya <rahul.kota@adya.ai>
2024-09-28 15:46:22 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Button variant="">Log in</Button>
+    </>
+  )
+}
+
+export default App
